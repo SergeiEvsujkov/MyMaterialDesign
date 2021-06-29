70eef832724e1f003edf0d8728e946869faed075
coffeemolkin <evsujkov@yandex.ru>
2021-06-29 17:00:37 +0700

    Homework1.2

--- app/src/main/java/com/example/mymaterialdesign/ui/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/mymaterialdesign/ui/main/PictureOfTheDayFragment.kt
@@ -1,20 +1,31 @@
 package com.example.mymaterialdesign.ui.main
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.*
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mymaterialdesign.R
 import com.example.mymaterialdesign.databinding.MainFragmentBinding
 import com.example.mymaterialdesign.ui.picture.PictureOfTheDayData
 
 import android.view.*
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import coil.api.load
+import com.example.mymaterialdesign.MainActivity
+import com.example.mymaterialdesign.ui.picture.BottomNavigationDrawerFragment
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.bottom_sheet_layout.*
+import kotlinx.android.synthetic.main.main_activity.*
 
 
 import kotlinx.android.synthetic.main.main_fragment.*
@@ -28,6 +39,8 @@ class PictureOfTheDayFragment : Fragment() {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,7 +52,22 @@ class PictureOfTheDayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getData().observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+        viewModel.getData().observe(viewLifecycleOwner, { renderData(it) })
+
+        input_layout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${input_edit_text.text.toString()}")
+            })
+        }
+
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        setBottomAppBar(view)
+
+        chipGroup.setOnCheckedChangeListener { chipGroup, position ->
+            chipGroup.findViewById<Chip>(position)?.let {
+                Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun renderData(data: PictureOfTheDayData) {
@@ -57,6 +85,8 @@ class PictureOfTheDayFragment : Fragment() {
                         error(R.drawable.ic_load_error_vector)
                         placeholder(R.drawable.ic_no_photo_vector)
                     }
+                    bottom_sheet_description_header.text = serverResponseData.title
+                    bottom_sheet_description.text = serverResponseData.explanation
                 }
             }
             is PictureOfTheDayData.Loading -> {
@@ -69,7 +99,73 @@ class PictureOfTheDayFragment : Fragment() {
         }
     }
 
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+//                    BottomSheetBehavior.STATE_DRAGGING -> TODO("not implemented")
+//                    BottomSheetBehavior.STATE_COLLAPSED -> TODO("not implemented")
+ //                   BottomSheetBehavior.STATE_EXPANDED -> TODO("not implemented")
+ //                   BottomSheetBehavior.STATE_HALF_EXPANDED -> TODO("not implemented")
+ //                   BottomSheetBehavior.STATE_HIDDEN -> TODO("not implemented")
+  //                  BottomSheetBehavior.STATE_SETTLING -> TODO("not implemented")
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+//                TODO("not implemented")
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> Toast.makeText(context, "Favourite", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_search -> Toast.makeText(context, "Search", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_settings -> Toast.makeText(context, "Settings", Toast.LENGTH_SHORT).show()
+            android.R.id.home -> {
+                activity?.let {
+                    BottomNavigationDrawerFragment().show(it.supportFragmentManager, "tag")
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
+
+        fab.setOnClickListener {
+            if (isMain) {
+                isMain = false
+                bottom_app_bar.navigationIcon = null
+                bottom_app_bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                fab.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_back_fab))
+                bottom_app_bar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            } else {
+                isMain = true
+                bottom_app_bar.navigationIcon =
+                    ContextCompat.getDrawable(context, R.drawable.ic_hamburger_menu_bottom_bar)
+                bottom_app_bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                fab.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_plus_fab))
+                bottom_app_bar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+        }
+    }
+
     companion object {
+        private var isMain = true
         fun newInstance() =
             PictureOfTheDayFragment()
     }
--- app/src/main/java/com/example/mymaterialdesign/ui/main/PictureOfTheDayViewModel.kt
+++ app/src/main/java/com/example/mymaterialdesign/ui/main/PictureOfTheDayViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.mymaterialdesign.ui.main
 
+import android.annotation.SuppressLint
+import android.icu.text.DateFormat.YEAR_ABBR_MONTH
+import android.icu.text.DateFormat.getPatternInstance
+import android.os.Build
+import android.text.format.DateFormat
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,6 +16,11 @@ import com.example.mymaterialdesign.ui.picture.PictureOfTheDayData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class PictureOfTheDayViewModel(
     private val liveDataForViewToObserve: MutableLiveData<PictureOfTheDayData> = MutableLiveData(),
@@ -22,13 +33,29 @@ class PictureOfTheDayViewModel(
         return liveDataForViewToObserve
     }
 
+
+    @SuppressLint("SimpleDateFormat")
     private fun sendServerRequest() {
         liveDataForViewToObserve.value = PictureOfTheDayData.Loading(null)
         val apiKey: String = BuildConfig.NASA_API_KEY
+
+        val date: String = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val current = LocalDateTime.now()
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+            current.format(formatter)
+
+        } else {
+            val dateNow = Date()
+            val formatter = SimpleDateFormat("yyyy-MM-dd")
+            formatter.format(dateNow)
+        }
+// Немного запарился с датой, вывел в лог :))
+        Log.d("dateSend", date)
+
         if (apiKey.isBlank()) {
             PictureOfTheDayData.Error(Throwable("You need API key"))
         } else {
-            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey).enqueue(object :
+            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey, date).enqueue(object :
                 Callback<PODServerResponseData> {
                 override fun onResponse(
                     call: Call<PODServerResponseData>,
