691e7a4d2bcbadcd0fcc7157e7f76d9319537227
coffeemolkin <evsujkov@yandex.ru>
2021-06-29 11:00:44 +0700

    Start

---
+++ app/src/main/java/com/example/mymaterialdesign/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mymaterialdesign
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mymaterialdesign.databinding.MainActivityBinding
+import com.example.mymaterialdesign.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+
+    }
+
+}
